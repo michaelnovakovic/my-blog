c0ddc53906c4a4b553166c7c82a76cef7a9be028
Michael Novakovic <michael076446@hotmail.com>
2020-03-22 13:18:37 -0400

    First Commit

---
+++ src/build/precache-manifest.e4744136a326891a53c54dd8b3c71445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5305c91d0b6ecd8458a6715b1c3fcdb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0b82bec555772b51041",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f1425ab659480fcd05e0",
+    "url": "/static/js/2.fe7aa150.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.fe7aa150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b82bec555772b51041",
+    "url": "/static/js/main.73e1b36c.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
